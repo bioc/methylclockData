2cb78cd4dd6a90f9e034cbd7218a552b2adef227
dpelegri <dpelegri@gmail.com>
2020-12-09 21:06:29 +0100

    V0.99.10

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -7,7 +7,7 @@
 
 rootdir <- getwd()
 
-system(paste0("wget https://github.com/perishky/meffil/",
+system32(paste0("wget https://github.com/perishky/meffil/",
        "blob/master/inst/references.rda?raw=true ./inst/extdata")
        )
 
