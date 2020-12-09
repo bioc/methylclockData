1de2da81c5929596400aa6b5205dbf95a9024bb2
dpelegri <dpelegri@gmail.com>
2020-12-09 19:47:14 +0100

    V0.99.6

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,7 @@
 # Load data
 
 .onLoad <- function(libname, pkgname) {
-   fl <- system.file("extdata", "metadata.csv", package="methylclockData")
-   titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
+   fl <- system.file("extdata", "metadata.csv", package = "methylclockData")
+   titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
    createHubAccessors(pkgname, titles)
-
 }
